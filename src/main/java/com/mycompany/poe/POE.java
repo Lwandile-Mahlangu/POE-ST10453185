8829fdd931ca1de871510ff78dbaff857d9dbdd6
RC_Student_lab <RC_Student_lab@10.0.0.136>
2024-09-18 12:48:15 +0000

    First Commit

---
+++ src/main/java/com/mycompany/poe/POE.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poe;
+import java.util.Scanner;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class POE {
+
+
+    public static void main(String[] args) {
+        //Declaration
+        
+        Scanner scanner = new Scanner(System.in);
+        
+          registerUser second = new registerUser();
+        
+        // Input username and password
+        System.out.println("Enter Username: ");
+        String username = scanner.nextLine();
+
+        System.out.println("Enter Password: ");
+        String password = scanner.nextLine();
+
+        // Input first name and last name for login greeting
+        System.out.println("Enter First Name: ");
+        String firstName = scanner.nextLine();
+
+        System.out.println("Enter Last Name: ");
+        String lastName = scanner.nextLine();
+
+      
+    }
+}
---
+++ src/main/java/com/mycompany/poe/registerUser.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.poe;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class registerUser {
+    String firstName;
+    String lastName;
+    String username;
+    String password;
+   
+    // Function to validate username
+    public static String validateUsername(String username) {
+        if (username.contains("_") && username.length() <= 5) {
+            return "Username successfully captured.";
+        } else {
+            return "Username is not correctly formatted, please ensure that your username contains an underscore and is no more than 5 characters in length.";
+        }
+    }
+   // Function to validate password
+    public static Boolean checkPassword(String password) {
+        
+        String capitalLetterPattern = ".*[A-Z].*";
+        String numberPattern = ".*[0-9].*";
+        String specialCharPattern = ".*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?].*";
+        
+        if (password.length() >= 8 
+                
+            && password.equals(capitalLetterPattern)
+            && password.equals(numberPattern)
+            && password.equals(specialCharPattern)) {
+            System.out.println("Password successfully captured.");
+            return true;
+        } else {
+            System.out.println(" \"Password is not correctly formatted, please ensure that the password contains at least 8 characters, a capital letter, a number, and a special character.");
+            
+            return false;
+        }
+    }  
+    public Boolean loginUser(){
+        if(
+            firstName.equals(firstName)
+                    && lastName.equals(lastName)
+                    && password.equals(password)
+                    && username.equals(username));
+        
+        System.out.println("Welcome "+ firstName +" "+ lastName+" it is great to see you again" );
+        return true;
+    }
+    
+}
+
