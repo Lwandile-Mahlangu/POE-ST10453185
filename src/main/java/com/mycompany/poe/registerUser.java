fda56874c5c2eb7850e4bdeb8181c75ab99bdc03
RC_Student_lab <RC_Student_lab@10.0.0.136>
2024-09-28 19:33:20 +0000

    Welcome the user

--- src/main/java/com/mycompany/poe/registerUser.java
+++ src/main/java/com/mycompany/poe/registerUser.java
@@ -3,72 +3,74 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.poe;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 /**
  *
  * @author RC_Student_lab
  */
 public class registerUser {
-   private String firstName;
-   private String lastName;
     private String username;
-   private String password;
+    private String password;
+    private String firstName;
+    private String lastName;
    
-   
-      
-   //Getters
-   public String getUsername(){
-       return username;
-   }
-    public String getPassword(){
-       return password;
-    }
-    public String getFirstName(){
-       return firstName;
-    }
-     
-    public String getLastName(){
-       return lastName;
-    }
-    
-    // Function to validate username
-    public static String validateUsername(String username) {
-        if (username.contains("_") && username.length() <= 5) {
-            return "Username successfully captured.";
-        } else {
-            return "Username is not correctly formatted, please ensure that your username contains an underscore and is no more than 5 characters in length.";
-        }
-    }
-   // Function to validate password
-    public static Boolean checkPassword(String password) {
-        
-        String capitalLetterPattern = ".*[A-Z].*";
-        String numberPattern = ".*[0-9].*";
-        String specialCharPattern = ".*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?].*";
-        
-        if (password.length() >= 8 
-                
-            && password.equals(capitalLetterPattern)
-            && password.equals(numberPattern)
-            && password.equals(specialCharPattern)) {
-            System.out.println("Password successfully captured.");
+// Method to validate the username
+    public boolean checkUserName(String username) {
+        if (username.length() <= 5 && username.contains("_")) {
+            System.out.println("Username successfully captured");
             return true;
         } else {
-            System.out.println("Password is not correctly formatted, please ensure that the password contains at least 8 characters, a capital letter, a number, and a special character.");
-            
+            System.out.println("Username is not correctly formatted, please ensure that your username contains an underscore and is no more than 5 characters in length.");
             return false;
         }
-    }  
-    public Boolean loginUser(){
-        if(
-            firstName.equals(firstName)
-                    && lastName.equals(lastName)
-                    && password.equals(password)
-                    && username.equals(username));
-        
-        System.out.println("Welcome "+ firstName +" "+ lastName+" it is great to see you again" );
-        return true;
     }
     
-}
+// Method to validate the password complexity
+    public boolean checkPasswordComplexity(String password) {
+        String pattern = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*]).{8,}$";
+        Pattern regex = Pattern.compile(pattern);
+        Matcher matcher = regex.matcher(password);
 
+        if (matcher.find()) {
+            System.out.println("Password successfully captured");
+            return true;
+        } else {
+            System.out.println("Password is not correctly formatted, please ensure that the password contains at least 8 characters, a capital letter, a number and a special character.");
+            return false;
+        }
+    }
+    
+        // Method to register the user
+ public String registerUser(String username, String password, String firstName, String lastName) {
+        if (!checkUserName(username)) {
+            return "Username is not correctly formatted, please ensure that your username contains an underscore and is no more than 5 characters in length.";
+        }
+        
+        if (!checkPasswordComplexity(password)) {
+            return "Password is not correctly formatted, please ensure that the password contains at least 8 characters, a capital letter, a number and a special character.";
+        }
+
+        // Store user details if both username and password are valid
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+
+        return "User registration successful!";
+    }
+     // Method to verify login credentials
+    public boolean loginUser(String username, String password) {
+        return this.username != null && this.password != null && this.username.equals(username) && this.password.equals(password);
+    }
+
+    // Method to return the login status message
+    public String returnLoginStatus(String username, String password) {
+        if (loginUser(username, password)) {
+            return "Welcome " + this.firstName + " " + this.lastName + ", it is great to see you again.";
+        } else {
+            return "Username or password incorrect, please try again.";
+        }
+    }
+}
